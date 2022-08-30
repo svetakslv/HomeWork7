8da60660eebf6b634102968e195a77691f688bad
Светка <sveta.chakrygina@mail.ru>
2022-08-30 16:49:05 +0300

    Задача №1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        ///Задача №1
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov";
+        String foolName = firstName + middleName + lastName;
+        System.out.println("Ф.И.О. сотрудника " + foolName);
+
+    }
+}
