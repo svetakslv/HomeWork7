0ab3cacd0c4ab2b4f71fc3ee5c6b863ba4aed271
Светка <sveta.chakrygina@mail.ru>
2022-08-30 18:01:26 +0300

    Задачи №1,2,3

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,19 @@ public class Main {
     public static void main(String[] args) {
         ///Задачи №1,2
         {
-            String firstName = "Ivan ";
-            String middleName = "Ivanovich ";
-            String lastName = "Ivanov ";
-            String foolName = lastName + firstName + middleName;
+            String firstName = "Ivan";
+            String middleName = "Ivanovich";
+            String lastName = "Ivanov";
+            String foolName = lastName + " " + firstName + " " + middleName;
             System.out.println("Ф.И.О. сотрудника " + foolName);
-            String foolName2 = "Ivanov Ivan Ivanovich";
-            System.out.println(foolName2.toUpperCase());
+            String foolName2 = foolName.toUpperCase();
+            System.out.println(foolName2);
         }
         ///Задача №3
-        String firstName = "Семён ";
-        String middleName = "Семёнович ";
-        String lastName = "Иванов ";
-        String foolName = lastName + firstName + middleName;
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String foolName = lastName + " " + firstName + " " + middleName;
         String foolName2 = foolName.replace('ё', 'е');
         System.out.println("Данные ФИО сотрудника — " + foolName2);
 
